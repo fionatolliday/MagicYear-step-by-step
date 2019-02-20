e2b0d9a7b7e5ed50734caa95176a34190caf46bf
Fiona Tolliday <fionatolliday@hotmail.com>
2019-02-21 09:49:18 +1100

    psuedo coding

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        print welcome message
+//        get users first name
+//        get users surname
+//        get users annual salary
+//        get users start year
+
+//        create method to return full name
+//        create method to return monthly salary
+//        create method to return magic year
+
+//        print magic details message
+//        print full name
+//        print monthly salary
+//        print magic year
+
+
+
+    }
+}
+
+
+
+////        Welcome to the magic year calculator!
+////
+////        Please input your name: John
+////        Please input your surname: Doe
+////        Please enter your annual salary: 60050
+////        Please enter your work start year : 1980
+////
+////        Your magic age details are:
+////
+////        Name: John Doe
+////        Monthly Salary: 5004
+////        Magic Year: 2045
